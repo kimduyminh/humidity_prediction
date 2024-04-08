22ff8540dc8c846954e7ce43af131dcde77b3b0a
kimduyminh <minhkimfp212@gmail.com>
2024-04-08 15:49:19 +0700

    update :)

--- xgboost_test.py
+++ xgboost_test.py
@@ -5,6 +5,7 @@ import pandas as pd
 from timeit import default_timer as timer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
+import os
 
 #import files and handling datas
 data=pd.read_csv("weather.csv")
@@ -23,3 +24,9 @@ prediction1=model_1.predict(x_test)
 print(mean_absolute_error(y_test,prediction1))
 end=timer()
 print(str(end-start)+"s")
+
+#model_2 (Optimizing a bit lol)
+start=timer()
+cpu_cores=os.cpu_count()-2
+model_2=XGBRegressor(random_state=0,n_estimator=1000,learning_rate=0.05,n_jobs=cpu_cores)
+model_2.fit(x_train,y_train,early_stopping_rounds=5,eval_set=[(x_test,y_test)])
