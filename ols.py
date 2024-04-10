2aa9fecd65425df071cb2bc5f928d4321057bc55
kimduyminh <minhkimfp212@gmail.com>
2024-04-10 22:10:54 +0700

    updatee

---
+++ ols.py
@@ -0,0 +1,54 @@
+<<<<<<< HEAD
+#import required libs
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+#import files and handling datas
+data=pd.read_csv("weather.csv")
+encoded_data=pd.get_dummies(data,columns=["province","wind_d","date"])
+
+#setting variables and target variable
+y=encoded_data.humidi
+x=encoded_data.drop('humidi', axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+#create model
+model=linear_model.LinearRegression()
+
+#train model
+model.fit(x_train,y_train)
+
+#print the model coefficients
+coef=model.coef_
+print(coef)
+#calculate the mean error
+prediction=model.predict(x_test)
+print("Mae: "+str(mean_absolute_error(y_test,prediction)))
+=======
+#import required libs
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+#import files and handling datas
+data=pd.read_csv("weather.csv")
+encoded_data=pd.get_dummies(data,columns=["province","wind_d","date"])
+
+#setting variables and target variable
+y=encoded_data.humidi
+x=encoded_data.drop('humidi', axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+#create model
+model=linear_model.LinearRegression()
+
+#train model
+model.fit(x_train,y_train)
+
+#calculate the mean error
+prediction=model.predict(x_test)
+print("Mae: "+mean_absolute_error(y_test,prediction))
+>>>>>>> 2193f69c8866bccf2b6ace78be85fe8edc439cda
